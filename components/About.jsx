a51c427e4b808ab5c51964e9c6e0af0d97c93d1f
devByScotty <155840062+devByScotty@users.noreply.github.com>
2024-01-20 20:15:44 +0200

    Update About.jsx

--- components/About.jsx
+++ components/About.jsx
@@ -13,15 +13,15 @@ const About = () => {
           </p>
           <h2 className='py-4'>Who I Am</h2>
           <p className='py-2 text-gray-600'>
-          I'm a dedicated junior developer, my primary focus revolves around creating mobile-responsive front-end UI applications 
+          I&apos;m a dedicated junior developer, my primary focus revolves around creating mobile-responsive front-end UI applications 
           that seamlessly integrate with APIs and backend technologies. I am driven by a passion for constant learning and staying 
           up-to-date with the latest advancements in the field. 
           I firmly believe that there are various approaches to problem-solving, and I am always open to exploring new methods.
           </p>
 
           <p className='py-2 text-gray-600'>
-          While my core skills lie in developing front-end applications using HTML, CSS, JavaScript, C++, and React, 
-          I am eager to expand my knowledge and proficiency in backend technologies. 
+          While my core skills lie in developing front-end application.
+          I&apos;m eager to expand my knowledge and proficiency in backend technologies. 
           I understand the importance of integrating backend functionalities into my projects to enhance their overall functionality and user experience. 
           I have a natural ability to quickly grasp new tech stacks, allowing me to adapt and learn backend technologies as needed.
           </p>
